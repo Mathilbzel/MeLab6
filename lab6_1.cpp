dd259460a3214e287708a2504d398613bb885c25
Mathilbzel <buachompoo_r@cmu.ac.th>
2023-12-01 15:43:37 +0700

    lab1edit7

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -6,15 +6,14 @@ int main(){
     cout << "Enter an integer: ";
     cin >> x;
     while(x!=0){
-        cout << "Enter an integer: ";
-        cin >> x;
         if(x%2==0){
         y = y+1 ;
         }
         else{
         z = z+1 ;
         }
-
+        cout << "Enter an integer: ";
+        cin >> x;
     }
     cout << "#Even numbers = "<< y <<"\n";
     cout << "#Odd numbers = "<< z ;
